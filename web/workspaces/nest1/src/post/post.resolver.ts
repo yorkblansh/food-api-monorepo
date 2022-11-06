a08837c1dda4ddfc19004db17b4aaf8b2ee5a242
Oleg Chepelev <yorkblansh@gmail.com>
2022-11-06 21:13:14 +0300

    fix import

--- web/workspaces/nest1/src/post/post.resolver.ts
+++ web/workspaces/nest1/src/post/post.resolver.ts
@@ -3,7 +3,7 @@ import { PostService } from './post.service'
 // import { CreatePostInput } from './dto/create-post.input'
 import { UpdatePostInput } from './dto/update-post.input'
 import { PostCreateInput } from '../@generated/prisma-nestjs-graphql/post/post-create.input'
-import { PostUpdateInput } from 'src/@generated/prisma-nestjs-graphql/post/post-update.input'
+import { PostUpdateInput } from '../@generated/prisma-nestjs-graphql/post/post-update.input'
 
 @Resolver('Post')
 export class PostResolver {
