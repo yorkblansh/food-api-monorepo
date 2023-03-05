2761992b4cfe43679247eb2c49fb9ca85dfe2204
Oleg Chepelev <yorkblansh@yandex.com>
2023-03-05 13:07:20 +0300

    web: nest: add @generated to gitignore

--- web/workspaces/nest/src/@generated/prisma-nestjs-graphql/tag/tag-avg-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class TagAvgAggregateInput {
-
-    @Field(() => Boolean, {nullable:true})
-    id?: true;
-}
--- web/workspaces/nest/src/@generated/prisma-nestjs-graphql/tag/tag-create-without-posts.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-
-@InputType()
-export class TagCreateWithoutPostsInput {
-
-    @Field(() => String, {nullable:false})
-    name!: string;
-}
